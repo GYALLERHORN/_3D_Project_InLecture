821c4fde28c487143709f897b49aa46e8afdb701
GYALLERHORN <kjins1998@gmail.com>
2023-10-06 18:38:58 +0900

    에셋 추가, 기본 UI 제작

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -111,9 +111,9 @@ public class PlayerController : MonoBehaviour
 
         for (int i = 0; i < rays.Length; i++)
         {
-            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask)) // Ray를 0.1길이만큼 쏴서 그 안에 groundLayerMask가 찍히면
             {
-                return true;
+                return true; // 점프 가능
             }
         }
 
