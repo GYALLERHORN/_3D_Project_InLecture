ea4bdde5bcfd0598f6d38d1005ea5b36ff1f7647
GYALLERHORN <kjins1998@gmail.com>
2023-10-06 17:21:42 +0900

    플레이어 기본 행동 기능 작성
    이동, 시야 전환, 점프

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,132 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("MoveMent")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumpforce;
+    public LayerMask groundLayerMask;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    [SerializeField]
+    [Range(0.01f, 5f)]
+    private float lookSensitivity;
+
+    private Vector2 mouseDelta;
+    [HideInInspector]
+    public bool canlook = true;
+
+    private Rigidbody _rigidbody;
+
+    public static PlayerController instance;
+
+    private void Awake()
+    {
+        instance = this;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        if (canlook)
+        {
+            CameraLook();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = _rigidbody.velocity.y;
+
+        _rigidbody.velocity = dir;
+    }
+
+    void CameraLook()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook); // ??
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * jumpforce, ForceMode.Impulse); 
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + Vector3.up * 0.01f, Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + Vector3.up * 0.01f, Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + Vector3.up * 0.01f, Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + Vector3.up * 0.01f, Vector3.down)
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawRay(transform.position + (transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (transform.right * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.right * 0.2f), Vector3.down);
     }
 }
